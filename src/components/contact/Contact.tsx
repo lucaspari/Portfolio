63b26baf575403a8c58efbf89f4434bb3f63fb2e
dumbleedore <lucas.pari@icloud.com>
2022-12-03 22:38:11 -0300

    finishing translation

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,20 +1,24 @@
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export const Contact = () => {
+  const { t } = useTranslation();
   return (
     <Box p={"2em"}>
       <Text fontSize={"3xl"}>Contact</Text>
       <Text className="text-lg">
-        If you want to reach me out please drop me an{" "}
+        {t("If you want to reach me out please drop me an")}{" "}
         <a
           href="mailto:lucaspariii@gmail.com"
           className="underline decoration-green-500"
         >
           email
         </a>
-        . You can also check out my social media presence following the links
-        below.
+        .{" "}
+        {t(
+          "You can also check out my social media presence following the links below."
+        )}
       </Text>
     </Box>
   );
